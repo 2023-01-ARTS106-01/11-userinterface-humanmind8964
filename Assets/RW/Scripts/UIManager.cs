b445dc3672834d5784ef8aa6807b79dbaa350ab4
Syc <humanminds-deathtoxijinping@yahoo.com>
2023-04-28 16:00:35 -0400

    1

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -9,6 +9,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     public Animator startButton;
     public Animator settingsButton;
+    public Animator dialog;
 
     public void StartGame() 
     {
@@ -19,5 +20,13 @@ public class UIManager : MonoBehaviour
     {
     startButton.SetBool("isHidden", true);
     settingsButton.SetBool("isHidden", true);
+    dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings() 
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
     }
 }
